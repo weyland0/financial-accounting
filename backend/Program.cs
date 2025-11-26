b5a6b57304b428c84372fe31aaa8287435fbe63a
weyland0 <philippbatkov@gmail.com>
2025-11-26 12:45:28 +0300

    add DB Context in DI

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using finacc.DataAccess;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
