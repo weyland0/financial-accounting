b287c02d4f5874af8abf3e45bb30bfcc4cff4d17
weyland0 <philippbatkov@gmail.com>
2025-11-26 12:43:58 +0300

    create a model for the user entity

---
+++ backend/Models/User.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace finacc.Models;
+
+
+[Table("users")]
+public class User
+{
+    [Key]
+    [Column("user_id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("email")]
+    public required string Email { get; set; }
+
+    [Required]
+    [Column("password_hash")]
+    public required string PasswordHash { get; set; }
+
+    [Required]
+    [Column("full_name")]
+    public required string FullName { get; set; }
+
+    [Column("role_id")]
+    public int? RoleId { get; set; }
+
+    [Column("company_id")]
+    public int? CompanyId { get; set; }
+
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Column("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+}
