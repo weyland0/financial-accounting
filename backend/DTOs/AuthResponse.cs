cfe6402887b8debfc6274b699fd4f4ff4dff9572
weyland0 <philippbatkov@gmail.com>
2025-11-30 19:34:33 +0300

    combine LoginResponse and RegisterResponse into AuthResponse

--- backend/DTOs/AuthResponse.cs
+++ backend/DTOs/AuthResponse.cs
@@ -1,7 +1,7 @@
 namespace finacc.DTOs;
 
 
-public class LoginResponse
+public class AuthResponse
 {
     public required string Token { get; set; }
     public required string RefreshToken { get; set; }
--- backend/DTOs/RegisterResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace finacc.DTOs;
-
-
-public class RegisterResponse
-{
-    public required bool Success { get; set; }
-    public required string Message { get; set; }
-    public UserDto? UserDto { get; set; }
-}
