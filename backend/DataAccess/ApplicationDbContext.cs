92af984fb7c03433128862baeac8ca2f03f371f5
weyland0 <philippbatkov@gmail.com>
2025-12-05 21:56:14 +0300

    FIX: add sets for the account and category entities

--- backend/DataAccess/ApplicationDbContext.cs
+++ backend/DataAccess/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Organization> Organizations { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Categories> Categories { get; set; }
 
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
