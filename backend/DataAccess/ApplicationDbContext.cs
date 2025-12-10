860d9a58e59ecb620df68fbb46147ba79489919b
weyland0 <philippbatkov@gmail.com>
2025-12-10 14:22:35 +0300

    add DbSet for the transaction entity in the context

--- backend/DataAccess/ApplicationDbContext.cs
+++ backend/DataAccess/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Categories> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
 
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
