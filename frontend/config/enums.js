5d3704078a013b4b3d203f5f5f2426ce7ba9ff85
weyland0 <philippbatkov@gmail.com>
2025-12-15 12:01:28 +0300

    create enum.js
    The place where all the enums of the system are stored.

---
+++ frontend/config/enums.js
@@ -0,0 +1,30 @@
+
+export const accountTypeLabels = {
+    cash:       'Наличные',
+    currency:   'Валютный счет',
+    payment:    'Расчетный счет',
+    individual: 'Индивидуальный счет'
+}
+
+export const accountTypeColors = {
+    cash:       '#2f8f3f',
+    currency:   '#0ca678',
+    payment:    '#1976d2',
+    individual: '#d63384'
+}
+
+export const currencies = {
+    RUB: 'RUB',
+    USD: 'USD',
+    EUR: 'EUR'
+}
+
+export const categoryTypeValues = {
+    income: 'INCOME',
+    expense: 'EXPENSE'
+}
+
+export const categoryTypeLabels = {
+    income: 'Доход',
+    expense: 'Расход'
+}
