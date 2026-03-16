8ea99574ab355a71820541af0e60416314b7d484
yaahochyest <philippbatkov@gmail.com>
2026-03-16 21:03:22 +0300

    add Role model to DB context

--- backend/DataAccess/ApplicationDbContext.cs
+++ backend/DataAccess/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Counterparty> Counterparties { get; set; }
     public DbSet<Invoice> Invoices { get; set; }
     public DbSet<Invite> Invites { get; set; }
+    public DbSet<Role> Roles { get; set; }
 
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
