3096445d48f834357d05ec53c167c2f5148df053
yaahochyest <philippbatkov@gmail.com>
2026-03-18 12:15:11 +0300

    use the names instead of the IDs of fields in InviteResponse

--- backend/DTOs/InviteResponse.cs
+++ backend/DTOs/InviteResponse.cs
@@ -5,8 +5,8 @@ public class InviteResponse
 {
     public required int Id { get; set; }
     public required string Token { get; set; }
-    public required int OrganizationId { get; set; }
-    public required int RoleId { get; set; }
+    public required string OrganizationName { get; set; }
+    public required string RoleName { get; set; }
     public bool IsRevoked { get; set; }
     public DateTime CreatedAt { get; set; }
 }
