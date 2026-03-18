e511a4ca6855daf97bbe01572d019a2dd6d53797
yaahochyest <philippbatkov@gmail.com>
2026-03-18 12:16:21 +0300

    add InviteAcceptRequest DTO

--- backend/DTOs/InviteRequest.cs
+++ backend/DTOs/InviteRequest.cs
@@ -7,3 +7,9 @@ public class InviteRequest
     public required int RoleId { get; set; }
     public bool IsRevoked { get; set; } = false;
 }
+
+
+public class InviteAcceptRequest
+{
+    public int UserId { get; set; }
+}
