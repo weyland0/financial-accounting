37470a7b985523bfe603389ce70fb8202ec1ca2e
yaahochyest <philippbatkov@gmail.com>
2026-03-24 14:18:06 +0300

    create RoleController

---
+++ backend/Controllers/RoleController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using finacc.Services;
+using finacc.DTOs;
+
+namespace FinanceApp.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class RoleController : ControllerBase
+{
+    private readonly IRoleService _service;
+
+
+    public RoleController(IRoleService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var result = await _service.GetById(id);
+        return result.ToActionResult();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _service.GetAll();
+        return result.ToActionResult();
+    }
+}
